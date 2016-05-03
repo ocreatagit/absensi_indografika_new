24818f6ccb320c294279ee265e12ad6cdec542d8
white-dnv <danielvitayasa@hotmail.com>
2016-05-03 21:11:12 +0700

    DAFTAR KARYAWAN PERHARI

--- app/controllers/DaftarController.php
+++ app/controllers/DaftarController.php
@@ -1,29 +1,115 @@
 <?php
 
 class DaftarController extends \BaseController {
-    
-    public function getTimeServer(){  
+
+    public function getTimeServer() {
         date_default_timezone_set('Asia/Jakarta');
         echo date('H:i:s');
     }
-    
-    public function getDateServer(){  
+
+    public function getDateServer() {
         date_default_timezone_set('Asia/Jakarta');
         echo date('d F Y');
     }
 
-    public function getDaftarMasuk(){
-        $sql = "SELECT mk01.idkar, mk01.nama, DATE_FORMAT(mj02.jmmsk, '%H:%i') as jmmsk, DATE_FORMAT(ta02.tglmsk, '%H:%i') as tglmsk,CAST(TIME_TO_SEC(TIMEDIFF(DATE_FORMAT(ta02.tglmsk, '%H:%i'), DATE_FORMAT(mj02.jmmsk, '%H:%i')))/60 as integer) as lbt  FROM mk01
-                RIGHT JOIN mj03 ON mj03.mk01_id = mk01.idkar
-                RIGHT JOIN mj02 ON mj02.idjk = mj03.mj02_id
-
+    public function getDaftarMasuk() {
+        $sqlMasuk = "SELECT mk01.idkar, 
+                       mk01.nama, 
+                       DATE_FORMAT(ta02.tglmsk, '%H:%i') as tglmsk
+                FROM mk01
                 RIGHT JOIN ta02 ON ta02.mk01_id = mk01.idkar
                 WHERE date(ta02.tglmsk) = date(NOW()) AND ta02.abscd = 0";
-        echo json_encode(DB::select(DB::raw($sql)));
+        $sqlPulang = "SELECT mk01.idkar, 
+                       mk01.nama, 
+                       DATE_FORMAT(ta02.tglmsk, '%H:%i') as tglmsk
+                FROM mk01
+                RIGHT JOIN ta02 ON ta02.mk01_id = mk01.idkar
+                WHERE date(ta02.tglmsk) = date(NOW()) AND ta02.abscd = 1";
+        $sqlKeluar = "SELECT mk01.idkar, 
+                       mk01.nama, 
+                       DATE_FORMAT(ta02.tglmsk, '%H:%i') as tglmsk
+                FROM mk01
+                RIGHT JOIN ta02 ON ta02.mk01_id = mk01.idkar
+                WHERE date(ta02.tglmsk) = date(NOW()) AND ta02.abscd = 2";
+        $sqlKembali = "SELECT mk01.idkar, 
+                       mk01.nama, 
+                       DATE_FORMAT(ta02.tglmsk, '%H:%i') as tglmsk
+                FROM mk01
+                RIGHT JOIN ta02 ON ta02.mk01_id = mk01.idkar
+                WHERE date(ta02.tglmsk) = date(NOW()) AND ta02.abscd = 3";
+
+        $dbMasuk = DB::select(DB::raw($sqlMasuk));
+        $dbPulang = DB::select(DB::raw($sqlPulang));
+        $dbKeluar = DB::select(DB::raw($sqlKeluar));
+        $dbKembali = DB::select(DB::raw($sqlKembali));
+
+        $cMasuk = 0;
+        $cPulang = 0;
+        $cKeluar = 0;
+        $cKembali = 0;
+
+        $cek = true;
+        $global_array = array();
+        do {
+            $array = array();
+            if (count($dbMasuk) > $cMasuk) {
+                array_push($array, $dbMasuk[$cMasuk]->idkar);
+                array_push($array, $dbMasuk[$cMasuk]->nama);
+                array_push($array, $dbMasuk[$cMasuk]->tglmsk);
+                $cMasuk++;
+            } else {
+                $cek = FALSE;
+            }
+
+            if (count($dbPulang) > $cPulang) {
+                if ($dbPulang[$cPulang]->idkar == $array[0]) {
+                    array_push($array, $dbPulang[$cPulang]->tglmsk);
+                    $cPulang++;
+                } else {
+                    array_push($array, "-");
+                }
+            } else {
+                array_push($array, "-");
+            }
+
+            if (count($dbKeluar) > $cKeluar) {
+                if ($dbKeluar[$cKeluar]->idkar == $array[0]) {
+                    array_push($array, $dbKeluar[$cKeluar]->tglmsk);
+                    $cKeluar++;
+                } else {
+                    array_push($array, "-");
+                }
+            } else {
+                array_push($array, "-");
+            }
+
+            if (count($dbKembali) > $cKembali) {
+                if ($dbKembali[$cKembali]->idkar == $array[0]) {
+                    array_push($array, $dbKembali[$cKembali]->tglmsk);
+                    $cKembali++;
+                } else {
+                    array_push($array, "-");
+                }
+            } else {
+                array_push($array, "-");
+            }
+            if ($cek) {
+                array_push($global_array, $array);
+            }
+        } while ($cek);
+
+
+
+        echo json_encode($global_array);
     }
-    
-    public function getDaftarPulang(){
-        $sql = "SELECT mk01.idkar, mk01.nama, DATE_FORMAT(mj02.jmklr, '%H:%i') as jmmsk, DATE_FORMAT(ta02.tglmsk, '%H:%i') as tglmsk,CAST(TIME_TO_SEC(TIMEDIFF(DATE_FORMAT(ta02.tglmsk, '%H:%i'), DATE_FORMAT(mj02.jmklr, '%H:%i')))/60 as integer) as lbt  FROM mk01
+
+    public function getDaftarPulang() {
+        $sql = "SELECT mk01.idkar, 
+                       mk01.nama, 
+                       DATE_FORMAT(mj02.jmklr, '%H:%i') as jmmsk, 
+                       DATE_FORMAT(ta02.tglmsk, '%H:%i') as tglmsk,
+                       CAST(TIME_TO_SEC(TIMEDIFF(DATE_FORMAT(ta02.tglmsk, '%H:%i'), DATE_FORMAT(mj02.jmklr, '%H:%i')))/60 as integer) as lbt  
+                FROM mk01
                 RIGHT JOIN mj03 ON mj03.mk01_id = mk01.idkar
                 RIGHT JOIN mj02 ON mj02.idjk = mj03.mj02_id
 
@@ -31,4 +117,5 @@ class DaftarController extends \BaseController {
                 WHERE date(ta02.tglmsk) = date(NOW()) AND ta02.abscd = 1";
         echo json_encode(DB::select(DB::raw($sql)));
     }
+
 }
--- app/views/daftar/masuk.blade.php
+++ app/views/daftar/masuk.blade.php
@@ -18,9 +18,10 @@
             <tr>
                 <td>No Absen</td>
                 <td>Nama Karyawan</td>
-                <td>Jadwal jam masuk</td>
-                <td>Login jam masuk</td>
-                <td>Keterlambatan</td>
+                <td>Jam Masuk</td>
+                <td>Jam Istirahat</td>
+                <td>Jam Kembali</td>
+                <td>Jam Pulang</td>
             </tr>
         </thead>
         <tbody id="tblMasuk">
@@ -48,23 +49,24 @@
         });
         //ambil data dari json
         $.getJSON('<?php echo action('DaftarController@getDaftarMasuk') ?>', function (data) {
-            var str = "";
-            if (data.length > 0) {
-                $.each(data, function (key, val) {
-                    str += "<tr>";
-                    str += "<td>" + val.idkar + "</td>";
-                    str += "<td>" + val.nama + "</td>";
-                    str += "<td>" + val.jmmsk + "</td>";
-                    str += "<td>" + val.tglmsk + "</td>";
-                    str += "<td>" + val.lbt + " menit</td>";
-                    str += "</tr>";
-                });
-            } else {
-                str += "<tr>";
-                str += "<td class='text-center' colspan='5'>No Data available in table </td>";
-                str += "</tr>";
-            }
-            $("#tblMasuk").html(str);
+           var str = "";
+           if (data.length > 0) {
+               $.each(data, function (key, val) {
+                   str += "<tr>";
+                   str += "<td>" + val[0] + "</td>";
+                   str += "<td>" + val[1] + "</td>";
+                   str += "<td>" + val[2] + "</td>";
+                   str += "<td>" + val[3] + "</td>";
+                   str += "<td>" + val[4] + "</td>";
+                   str += "<td>" + val[5] + "</td>";
+                   str += "</tr>";
+               });
+           } else {
+               str += "<tr>";
+               str += "<td class='text-center' colspan='5'>No Data available in table </td>";
+               str += "</tr>";
+           }
+           $("#tblMasuk").html(str);
         });
     }, 10);
 </script>
